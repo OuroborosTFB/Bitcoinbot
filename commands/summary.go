738a7daafa51ff913a6260fbfc82100f70165970
OuroborosTFB <hmmm.true@gmail.com>
2022-02-13 14:17:06 +0300

    add commands, fixed model

---
+++ commands/summary.go
@@ -0,0 +1,20 @@
+package commands
+
+import (
+	"fmt"
+	"math"
+
+	"github.com/OuroborosTFB/Bitcoinbot/utils"
+)
+
+func GetSummary() (string, string, error) {
+	price, err := utils.GetApiCall()
+	last := price.Last
+	buy := price.Buy
+	his := ((last - buy) / buy) * 100
+	if !math.Signbit(float64(his)) {
+		return fmt.Sprintf("%.2f", price.Last), "%" + fmt.Sprintf("%.2f", ((last-buy)/buy)*100), err
+	} else {
+		return fmt.Sprintf("%.2f", price.Last), "-%" + fmt.Sprintf("%.2f", -1*((last-buy)/buy)*100), err
+	}
+}
